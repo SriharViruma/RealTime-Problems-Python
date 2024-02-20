ab4485704071dc057b872f8741ab867e739b9be4
Srihar Viruma M <99681366+SriharViruma@users.noreply.github.com>
2024-02-20 23:10:25 +0530

    Add files via upload

--- DAC_Binary_weighted.py
+++ DAC_Binary_weighted.py
@@ -1,6 +1,7 @@
 while True:
+        #input values will be stored in integer variables
         b3 = int(input("Enter B3: "))
-        b2 = int(input("Enter B2: "))
+        b2 = int(input("Enter B2: "))   
         b1 = int(input("Enter B1: "))
         b0 = int(input("Enter B0: "))
 
@@ -11,8 +12,10 @@ while True:
         #Formula For Volatge Present at its non - inventing input terminal
         vo = -(vref*rf)/r * (b3/(2**0)+b2/(2**1)+b1/(2**2)+b0/(2**3))
 
+        #Displaying the calculated value stored in Vo
         print(vo)
 
+        #To Break Loop
         continue_program = input("Do you want to continue? (y/n): ")
         if continue_program.lower() != "y":
             break
